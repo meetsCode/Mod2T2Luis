ecb4ff2bc41ec7f8e8ccdc5fc0309ccc8e8738cf
Luis Sanchez Peña <sanchezlsp@gmail.com>
2017-08-20 22:24:41 +0200

    uno absorber y análisis de tabla

--- absorcionDatos.R
+++
@@ -1,6 +0,0 @@
-
-dataRawAntiguos <- read.csv("/Users/luis/Desktop/existing product attributes.csv", stringsAsFactors=FALSE)
-dataRaw <- dataRawAntiguos 
-# dataRawNuevos <- read.csv("/Users/luis/Desktop/new product attributesII.csv", stringsAsFactors=FALSE)
-# 
-# dataRaw <- rbind(dataRawAntiguos, dataRawNuevos)
--- absorcionYPreparacionDatos.R
+++ absorcionYPreparacionDatos.R
@@ -1,4 +1,12 @@
 
+dataRawAntiguos <- read.csv("/Users/luis/Desktop/existing product attributes.csv", stringsAsFactors=FALSE)
+dataRaw <- dataRawAntiguos 
+# dataRawNuevos <- read.csv("/Users/luis/Desktop/new product attributesII.csv", stringsAsFactors=FALSE)
+# 
+# dataRaw <- rbind(dataRawAntiguos, dataRawNuevos)
+
+
+
 # lo que hago aquí es observar los datos gráfica y visualmente para 
 # limpiarlos.
 # solo cambio nombre de columnas, busco instancias (líneas) erróneas y 
